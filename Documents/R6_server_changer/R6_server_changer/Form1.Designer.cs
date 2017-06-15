c378ab00456b427d58b8846e2aeaa041fe576327
hth225 <hth225@naver.com>
2017-06-15 12:10:03 +0900

    first commit

---
+++ Documents/R6_server_changer/R6_server_changer/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace R6_server_changer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.radioButton11 = new System.Windows.Forms.RadioButton();
+            this.radioButton12 = new System.Windows.Forms.RadioButton();
+            this.radioButton13 = new System.Windows.Forms.RadioButton();
+            this.radioButton14 = new System.Windows.Forms.RadioButton();
+            this.radioButton15 = new System.Windows.Forms.RadioButton();
+            this.radioButton16 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(23, 79);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Auto";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter GameSetting.ini file locaion ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(170, 556);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Apply";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(265, 556);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Cancel";
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(23, 270);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(108, 19);
+            this.radioButton6.TabIndex = 8;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Brasil South";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(23, 309);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(113, 19);
+            this.radioButton7.TabIndex = 9;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Europe North";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(23, 353);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(113, 19);
+            this.radioButton8.TabIndex = 10;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Europe West";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(23, 395);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(90, 19);
+            this.radioButton9.TabIndex = 11;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "Asia East";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(23, 437);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(134, 19);
+            this.radioButton10.TabIndex = 12;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.Text = "Asia South East";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            // 
+            // radioButton11
+            // 
+            this.radioButton11.AutoSize = true;
+            this.radioButton11.Location = new System.Drawing.Point(23, 480);
+            this.radioButton11.Name = "radioButton11";
+            this.radioButton11.Size = new System.Drawing.Size(117, 19);
+            this.radioButton11.TabIndex = 13;
+            this.radioButton11.TabStop = true;
+            this.radioButton11.Text = "Australia East";
+            this.radioButton11.UseVisualStyleBackColor = true;
+            // 
+            // radioButton12
+            // 
+            this.radioButton12.AutoSize = true;
+            this.radioButton12.Location = new System.Drawing.Point(23, 518);
+            this.radioButton12.Name = "radioButton12";
+            this.radioButton12.Size = new System.Drawing.Size(105, 19);
+            this.radioButton12.TabIndex = 14;
+            this.radioButton12.TabStop = true;
+            this.radioButton12.Text = "Japan West";
+            this.radioButton12.UseVisualStyleBackColor = true;
+            // 
+            // radioButton13
+            // 
+            this.radioButton13.AutoSize = true;
+            this.radioButton13.Location = new System.Drawing.Point(23, 116);
+            this.radioButton13.Name = "radioButton13";
+            this.radioButton13.Size = new System.Drawing.Size(82, 19);
+            this.radioButton13.TabIndex = 20;
+            this.radioButton13.TabStop = true;
+            this.radioButton13.Text = "US East";
+            this.radioButton13.UseVisualStyleBackColor = true;
+            // 
+            // radioButton14
+            // 
+            this.radioButton14.AutoSize = true;
+            this.radioButton14.Location = new System.Drawing.Point(23, 152);
+            this.radioButton14.Name = "radioButton14";
+            this.radioButton14.Size = new System.Drawing.Size(98, 19);
+            this.radioButton14.TabIndex = 21;
+            this.radioButton14.TabStop = true;
+            this.radioButton14.Text = "US Central";
+            this.radioButton14.UseVisualStyleBackColor = true;
+            // 
+            // radioButton15
+            // 
+            this.radioButton15.AutoSize = true;
+            this.radioButton15.Location = new System.Drawing.Point(23, 190);
+            this.radioButton15.Name = "radioButton15";
+            this.radioButton15.Size = new System.Drawing.Size(142, 19);
+            this.radioButton15.TabIndex = 22;
+            this.radioButton15.TabStop = true;
+            this.radioButton15.Text = "US South Central";
+            this.radioButton15.UseVisualStyleBackColor = true;
+            // 
+            // radioButton16
+            // 
+            this.radioButton16.AutoSize = true;
+            this.radioButton16.Location = new System.Drawing.Point(23, 230);
+            this.radioButton16.Name = "radioButton16";
+            this.radioButton16.Size = new System.Drawing.Size(87, 19);
+            this.radioButton16.TabIndex = 23;
+            this.radioButton16.TabStop = true;
+            this.radioButton16.Text = "US West";
+            this.radioButton16.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 597);
+            this.Controls.Add(this.radioButton16);
+            this.Controls.Add(this.radioButton15);
+            this.Controls.Add(this.radioButton14);
+            this.Controls.Add(this.radioButton13);
+            this.Controls.Add(this.radioButton12);
+            this.Controls.Add(this.radioButton11);
+            this.Controls.Add(this.radioButton10);
+            this.Controls.Add(this.radioButton9);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Rainbow6 : Siege Data Center Changer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.RadioButton radioButton11;
+        private System.Windows.Forms.RadioButton radioButton12;
+        private System.Windows.Forms.RadioButton radioButton13;
+        private System.Windows.Forms.RadioButton radioButton14;
+        private System.Windows.Forms.RadioButton radioButton15;
+        private System.Windows.Forms.RadioButton radioButton16;
+    }
+}
+
---
+++ Documents/R6_server_changer/R6_server_changer/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace R6_server_changer
+{
+    public partial class Form1 : Form
+    {
+
+        string dac;
+        string fileloc;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void filewr(string dac, string fileloc)
+        {
+            
+            fileloc = "C:/Users/LAD/Documents/My Games/Rainbow Six - Siege/88b82571-82f9-49bd-a0df-7576dd6f39b2/GameSettings.ini";
+            Stream filest = new FileStream(fileloc, FileMode.Open);
+            StreamReader strd = new StreamReader(filest);
+            string content = strd.ReadToEnd();
+            content = content.Replace("DataCenterHint=default", "DataCenterHint=" + dac);
+            StreamWriter stwr = new StreamWriter(filest);
+            stwr.Write(content);
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RadioButton rdbt = sender as RadioButton;
+            
+            if (rdbt.Checked == false)
+            {
+                return;
+            }
+            switch(rdbt.Text)
+            {
+                case "Auto":
+                    {
+                        dac = "default";
+                        break;
+                    }
+                case "US East":
+                    {
+                        dac = "eus";
+                        break;
+                    }
+                case "US Central":
+                    {
+                        dac = "cus";
+                        break;
+                    }
+                case "US South Central":
+                    {
+                        dac = "scus";
+                        break;
+                    }
+                case "US West":
+                    {
+                        dac = "wus";
+                        break;
+                    }
+                case "Brasil South":
+                    {
+                        dac = "sbr";
+                        break;
+                    }
+                case "Europe Notrh":
+                    {
+                        dac = "neu";
+                        break;
+                    }
+                case "Europe West":
+                    {
+                        dac = "weu";
+                        break;
+                    }
+                case "Asia East":
+                    {
+                        dac = "eas";
+                        break;
+                    }
+                case "Asia South East":
+                    {
+                        dac = "seas";
+                        break;
+                    }
+                case "Australia East":
+                    {
+                        dac = "aeu";
+                        break;
+                    }
+                case "Japan West":
+                    {
+                        dac = "wja";
+                        break;
+                    }
+                
+
+            }
+        }
+        /*
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Stream filelocation = new FileStream("fileloc.txt", FileMode.OpenOrCreate);
+            filelocation.Write(sender);
+
+        }
+        */
+
+    }
+}
